fc59b68edeb7c20199b9eff5f5b85b12be2305b7
Akhil Samarth <akhilsamarth236@gmail.com>
2020-01-08 01:35:55 -0500

    Created test triangle to make sure shaders, drawing, etc. are working before proceedng.

--- src/main.cpp
+++ src/main.cpp
@@ -31,11 +31,35 @@ int main(void)
 	shader.addShader("src/shader_fragment.glsl", GL_FRAGMENT_SHADER);
 	shader.linkProgram();
 
+	// create vbo for triangle
+	unsigned int vboId;
+	glGenBuffers(1, &vboId);
+	glBindBuffer(GL_ARRAY_BUFFER, vboId);
+
+	float data[] = {
+		0, 0.5, 0, 1, 0, 0,
+		-0.5, -0.5, 0, 0, 1, 0,
+		0.5, -0.5, 0, 0, 0, 1
+	};
+
+	// add data to buffer
+	glBufferData(GL_ARRAY_BUFFER, sizeof(data), data, GL_STATIC_DRAW);
+
+	// vertex attributes
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*) 0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*) (3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		// draw
+		glUseProgram(shader.getProgramId());
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
