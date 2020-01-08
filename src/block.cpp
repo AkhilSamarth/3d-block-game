de6c90283d23ebcdc13cd80e555cee430667f669
Akhil Samarth <akhilsamarth236@gmail.com>
2020-01-08 17:57:48 -0500

    Implemented Block class. Made changes to file structure so all non-code, non-exe files are in a single folder (assetts).

---
+++ src/block.cpp
@@ -0,0 +1,113 @@
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "block.h"
+
+// initalize static members
+unsigned int Block::vaoId = 0;
+bool Block::vaoInit = false;
+
+void Block::initVao() {
+	// create and bind a VAO
+	glGenVertexArrays(1, &vaoId);
+	glBindVertexArray(vaoId);
+
+	// create and bind the VBO for this VAO
+	unsigned int bufferId;
+	glGenBuffers(1, &bufferId);
+	glBindBuffer(GL_ARRAY_BUFFER, bufferId);
+
+	// data for a 1x1x1 cube with appropriate texture coordinates
+	Vertex verts[36] = {
+		// position				// texture coords
+		// top
+		-0.5, 0.5, -0.5,		-1, 1,
+		0.5, 0.5, -0.5,			1, 1,
+		0.5, 0.5, 0.5,			1, -1,
+
+		-0.5, 0.5, -0.5,		-1, 1,
+		0.5, 0.5, 0.5,			1, -1,
+		-0.5, 0.5, 0.5,			-1, -1,
+
+		// bottom
+		-0.5, -0.5, -0.5,		-1, 1,
+		0.5, -0.5, -0.5,		1, 1,
+		0.5, -0.5, 0.5,			1, -1,
+
+		-0.5, -0.5, -0.5, 		-1, 1,
+		0.5, -0.5, 0.5,			1, -1,
+		-0.5, -0.5, 0.5,		-1, -1,
+
+		// front
+		-0.5, 0.5, 0.5,			-1, 1,
+		0.5, 0.5, 0.5,			1, 1,
+		0.5, -0.5, 0.5,			1, -1,
+
+		-0.5, 0.5, 0.5,			-1, 1,
+		0.5, -0.5, 0.5,			1, -1,
+		-0.5, -0.5, 0.5,		-1,	-1,
+
+		// back
+		-0.5, 0.5, -0.5,		-1, 1,
+		0.5, 0.5, -0.5,			1, 1,
+		0.5, -0.5, -0.5, 		1, -1,
+
+		-0.5, 0.5, -0.5,		-1, 1,
+		0.5, -0.5, -0.5,		1, -1,
+		-0.5, -0.5, -0.5,		-1,	-1,
+
+
+		// right
+		0.5, 0.5, 0.5,			1, 1,
+		0.5, 0.5, -0.5,			-1, 1,
+		0.5, -0.5, -0.5,		-1, -1,
+
+		0.5, 0.5, 0.5,			1, 1,
+		0.5, -0.5, -0.5,		-1, -1,
+		0.5, -0.5, 0.5,			1, -1,
+
+		// left
+		-0.5, 0.5, 0.5,			1, 1,
+		-0.5, 0.5, -0.5,		-1, 1,
+		-0.5, -0.5, -0.5,		-1, -1
+
+		-0.5, 0.5, 0.5, 		1, 1,
+		-0.5, -0.5, -0.5,		-1, -1
+		-0.5, -0.5, 0.5,		1, -1,
+	};
+
+	// fill buffer
+	glBufferData(GL_ARRAY_BUFFER, 36 * sizeof(Vertex), verts, GL_STATIC_DRAW);
+
+	// vertex attributes
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*) 0);	// position
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*) (3 * sizeof(float)));	// texture coordinates
+	glEnableVertexAttribArray(1);
+
+	vaoInit = true;
+}
+
+Block::Block(int x, int y, int z, unsigned char* texture) : texture(texture) {
+	pos = glm::vec3(x, y, z);
+}
+
+glm::mat4 Block::getModelMatrix() {
+	glm::mat4 matrix = glm::mat4(1.0f);		// identity matrix
+	glm::translate(matrix, glm::vec3(pos));		// translate to correct position
+
+	return matrix;
+}
+
+void Block::bindTexture() {
+	return;		// TODO: implement this
+}
+
+void Block::bindVao() {
+	// if the vao doesn't exist yet, create it
+	if (!vaoInit) {
+		initVao();
+	}
+
+	glBindVertexArray(vaoId);
+}
---
+++ src/block.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+#include "drawing.h"
+
+class Block {
+private:
+	unsigned char* texture;	// pointer to texture data
+	glm::ivec3 pos;	// position of back, left, bottom corner (lowest x, y, z) along integer grid
+
+	static unsigned int vaoId;	// VAO for a single block (cube)
+	static bool vaoInit;	// whether or not the block VAO has been created
+
+	static void initVao();	// initalize the VAO
+public:
+	Block(int x, int y, int z, unsigned char* texture);		// position set to (x, y, z)
+	glm::mat4 getModelMatrix();		// returns the model matrix with the correct block position
+	void bindTexture();		// binds this block's texture
+
+	static void bindVao();		// binds the block VAO
+};
