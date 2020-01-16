342da2f77d683e78b21e61a96f90dd7744c12e2e
Akhil Samarth <akhilsamarth236@gmail.com>
2020-01-15 21:10:27 -0500

    Implemented code in Chunk for adding neighbor chunk.

--- src/chunk.cpp
+++ src/chunk.cpp
@@ -1,4 +1,39 @@
+#include <iostream>
 
 #include "chunk.h"
 
-Chunk::Chunk(glm::vec3 pos) : pos(pos), blocks(), neighborChunks() {}
+Chunk::Chunk(glm::vec2 pos) : pos(glm::vec3(pos.x, 0, pos.y)), blocks(), neighborChunks() {}
+
+void Chunk::generateBlocks() {}
+
+void Chunk::updateBlockFaces() {}
+
+void Chunk::addNeighbor(Chunk* chunk) {
+	// get the position of the neighbor chunk
+	glm::vec3 chunkPos = chunk->getPosition();
+
+	// make sure the neighbor is actually next to this chunk
+	// must be either same x/diff z or same z/diff x
+	if ((chunkPos.x == pos.x && chunkPos.z == pos.z) || (chunkPos.x != pos.x && chunkPos.z != pos.z)) {
+		std::cerr << "Incorrect neighbor chunk given!" << std::endl;
+		return;
+	}
+
+	// figure our which chunk this is and add it to the right spot
+	if (chunkPos.z < pos.z) {
+		// front
+		neighborChunks[0] = chunk;
+	}
+	else if (chunkPos.x > pos.x) {
+		// right
+		neighborChunks[1] = chunk;
+	}
+	else if (chunkPos.z > pos.z) {
+		// back
+		neighborChunks[2] = chunk;
+	}
+	else {
+		// left
+		neighborChunks[3] = chunk;
+	}
+}
--- src/chunk.h
+++ src/chunk.h
@@ -12,12 +12,12 @@ class Chunk {
 private:
 	Block* blocks[CHUNK_SIZE][WORLD_HEIGHT][CHUNK_SIZE];	// pointers to all blocks in this chunk at correct position
 	Chunk* neighborChunks[4];		// pointers to surrounding chunks in order (front, right, back, left)
-	glm::vec3 pos;		// position of left, bottom, front corner (lowest x, y, z) along integer grid (must be multiple of CHUNK_SIZE)
+	glm::vec3 pos;		// position of left, front corner (lowest x, z, y always 0) along integer grid (must be multiple of CHUNK_SIZE)
 public:
-	Chunk(glm::vec3 pos);	// create a chunk at this position
+	Chunk(glm::vec2 pos);	// create a chunk at the given (x, z)
 
+	glm::vec3 getPosition();	// returns the position of this chunk
 	void generateBlocks();		// fill blocks
 	void updateBlockFaces();	// set which faces of each block are exposed
-
 	void addNeighbor(Chunk* chunk);		// add a neighboring chunk
 };
