4afec428ce683d454e0a25b7b7b85a535cf514e3
Akhil Samarth <akhilsamarth236@gmail.com>
2020-01-15 22:52:44 -0500

    Added vector to keep track of this chunk's vertices.

--- src/chunk.cpp
+++ src/chunk.cpp
@@ -2,7 +2,27 @@
 
 #include "chunk.h"
 
-Chunk::Chunk(glm::vec2 pos) : pos(glm::vec3(pos.x, 0, pos.y)), blocks(), neighborChunks() {}
+void Chunk::addBlock(Block* block, int x, int y, int z) {
+
+}
+
+Block* Chunk::removeBlock(int x, int y, int z) {}
+
+Chunk::Chunk(glm::ivec2 pos) : blocks(), neighborChunks(), verts(std::vector<Vertex>()) {
+	// check position
+	if (pos.x % CHUNK_SIZE != 0 || pos.y % CHUNK_SIZE != 0) {
+		std::cerr << "Invalid chunk position (x: " << pos.x << ", z: " << pos.y << ") given!" << std::endl;
+		std::cerr << "This chunk will not work as expected!" << std::endl;
+
+		return;
+	}
+
+	// set position
+	this->pos = glm::ivec3(pos.x, 0, pos.y);
+
+	// add to chunkList
+	chunkList[pos] = this;
+}
 
 void Chunk::generateBlocks() {}
 
@@ -35,6 +55,43 @@ void Chunk::updateBlockFaces() {
 				}
 				if (z - 1 >= 0 && (neighbor = blocks[x][y][z - 1]) != nullptr) {
 					neighbor->boolFace(BIT_FACE_BACK, exposed);
+				}
+			}
+		}
+	}
+}
+
+void Chunk::updateVerts() {
+	verts.clear();
+
+	// loop through all block positions
+	for (int x = 0; x < CHUNK_SIZE; x++) {
+		for (int z = 0; z < CHUNK_SIZE; z++) {
+			for (int y = 0; y < WORLD_HEIGHT; y++) {
+				// if there's no block here, continue
+				Block* block;
+				if ((block = blocks[x][y][z]) == nullptr) {
+					continue;
+				}
+
+				// add exposed faces
+				if (block->getFace(BIT_FACE_TOP)) {
+					verts.insert(verts.end(), Block::TOP_FACE, Block::TOP_FACE + 6);
+				}
+				if (block->getFace(BIT_FACE_BOTTOM)) {
+					verts.insert(verts.end(), Block::BOTTOM_FACE, Block::BOTTOM_FACE + 6);
+				}
+				if (block->getFace(BIT_FACE_LEFT)) {
+					verts.insert(verts.end(), Block::LEFT_FACE, Block::LEFT_FACE + 6);
+				}
+				if (block->getFace(BIT_FACE_RIGHT)) {
+					verts.insert(verts.end(), Block::RIGHT_FACE, Block::RIGHT_FACE + 6);
+				}
+				if (block->getFace(BIT_FACE_FRONT)) {
+					verts.insert(verts.end(), Block::FRONT_FACE, Block::FRONT_FACE + 6);
+				}
+				if (block->getFace(BIT_FACE_BACK)) {
+					verts.insert(verts.end(), Block::BACK_FACE, Block::BACK_FACE + 6);
 				}
 			}
 		}
--- src/chunk.h
+++ src/chunk.h
@@ -1,23 +1,32 @@
 #pragma once
 
-#include <vector>
+#include <map>
+
 #include <glm/glm.hpp>
 
 #include "block.h"
+#include "drawing.h"
 
 #define CHUNK_SIZE 8		// each chunk will be a column with this length and width
 #define WORLD_HEIGHT 32		// height of the world 
 
 class Chunk {
 private:
+	static std::map<glm::ivec2, Chunk*> chunkList;		// a list of all the chunks mapped using chunk (x, z) position
+
 	Block* blocks[CHUNK_SIZE][WORLD_HEIGHT][CHUNK_SIZE];	// pointers to all blocks in this chunk at correct position
 	Chunk* neighborChunks[4];		// pointers to surrounding chunks in order (front, right, back, left)
-	glm::vec3 pos;		// position of left, front corner (lowest x, z, y always 0) along integer grid (must be multiple of CHUNK_SIZE)
+	glm::ivec3 pos;		// position of left, front corner (lowest x, z, y always 0) along integer grid (must be multiple of CHUNK_SIZE)
+	std::vector<Vertex> verts;	// all vertices of all faces which should be drawn of blocks in this chunk
 public:
-	Chunk(glm::vec2 pos);	// create a chunk at the given (x, z)
+	static void addBlock(Block* block, int x, int y, int z);	// add block to correct chunk at position (x, y, z) in global coords
+	static Block* removeBlock(int x, int y, int z);	// remove and return the block at (x, y, z) in global coords
 
-	glm::vec3 getPosition();	// returns the position of this chunk
-	void generateBlocks();		// fill blocks
+	Chunk(glm::ivec2 pos);	// create a chunk at the given (x, z)
+	glm::ivec3 getPosition();	// returns the position of this chunk
+	void generateBlocks();		// TODO: randomly generate blocks
 	void updateBlockFaces();	// set which faces of each block are exposed
+	void updateVerts();		// update the verts vector with the correct vertices
+	std::vector<Vertex> getVertices();		// return the vertices array
 	void addNeighbor(Chunk* chunk);		// add a neighboring chunk
 };
